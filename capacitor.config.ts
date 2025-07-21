eec0c2e9b5159678bea68a482e1b11cf0159bc38
chungvh <chungvh@brain-net.co.jp>
2025-07-21 16:13:47 +0700

    fix check name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,8 @@
 const config = {
   appId: 'com.example.nuxtapp',
   appName: 'NuxtQuasarApp',
-  webDir: '.output/public', // ✅ sửa tại đây
+  // webDir: '.output/public', // ✅ sửa tại đây
+  webDir: '.vercel/output/static', // ✅ sửa tại đây
   bundledWebRuntime: false
 }
 export default config;
