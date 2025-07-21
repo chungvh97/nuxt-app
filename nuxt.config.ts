cb97ff21f18639705b85356d3581063c66fea1ab
chungvh <chungvh@brain-net.co.jp>
2025-07-21 16:40:45 +0700

    fix check name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   nitro: {
-    preset: 'vercel' // phải có preset hoặc adapter phù hợp
+    // preset: 'vercel' // phải có preset hoặc adapter phù hợp
   },
   app: {
     head: {
